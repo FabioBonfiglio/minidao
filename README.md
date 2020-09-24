62023a0a23da9cadac0597c86c149172ca7e73f0
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2020-09-24 20:23:01 +0200

    Première implémentation Minidao (non testée)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Minidao
-Une implémentation de démonstration d'un smart contract / _mini-DAO_ gérant la distribution de fonds à des bénéficiaires à intervalles réguliers.
+Une implémentation de démonstration d'un _smart contract_ / _mini-DAO_ gérant la distribution de fonds à des bénéficiaires à intervalles réguliers.
 
 Cette démonstration fait partie du cours suivant: https://www.he-arc.ch/gestion/blockchain.
 
@@ -12,11 +12,11 @@ Cette démonstration fait partie du cours suivant: https://www.he-arc.ch/gestion
 ## Gouvernance
 Cette "_mini-DAO_" modélise la gouvernance suivante (l'implémentation du contrat a été réalisée d'après le cahier des charges suivant):
 - Un Comité (limité à 10 membres) approuve les votants qui pourront participer. Au moins 50% des Membres au Comité doivent approuver un votant pour que ce dernier puisse voter.
-- Toute personne est libre de faire une "donation" à la DAO. Les fonds sont détenus par le _smart contract_
+- Toute personne est libre de faire une "donation" à la DAO. Les fonds sont détenus par le contrat.
 - Chaque semaine, les votants peuvent nominer un bénéficiaire de leur choix, parmi une liste définie par les Membres au Comité (à l'unanimité).
-- Les fonds sont ensuite distribués, maximum une fois par semaine, aux bénéficiaires proportionnellement aux nombre de votes qu'ils auront récoltés.
+- Les fonds sont ensuite distribués, maximum une fois par semaine, aux bénéficiaires proportionnellement aux nombre de votes qu'ils auront récoltés. Le Membre au Comité qui aura déclenché la transaction sera automatiquement dédommagé par le contrat pour ses frais (annoncés), à hauteur de maximum 10% du montant de la cagnotte.
 - Après chaque distribution, les votes sont remis à zéro.
-- Le Comité peut décider, à l'unanimité uniquement, de terminer la DAO, en votant pour un bénéficiaire final du solde des fonds restants dans le contrat. Le contrat sera rendu inactif une fois cette opération terminée.
+- Le Comité peut décider, à l'unanimité uniquement, de terminer la DAO. Le solde des fonds détenus par le contrat sera versé à l'administrateur actuel. Le contrat sera rendu inactif une fois cette opération terminée.
 
 ## Code source
 Si vous voulez uniquement consulter le code source du contrat, il s'agit du fichier [`contracts/Minidao.sol`](contracts/Minidao.sol).
@@ -42,17 +42,19 @@ Une fois fait, on peut alors soit lancer une blockchain locale de tests, avec la
 ```sh
 $ npm run testchain
 ```
-et déployer ensuite le contrat dessus:
+et lancer les tests de fonctionnement, ce qui va au préalable déployer une nouvelle instance du contrat sur cette chaîne:
 ```sh
-$ npm run migrate
+$ npm test
 ```
 Il est ensuite possible d'interagir avec le smart contract via une [abstraction de contrat Truffle](https://www.trufflesuite.com/docs/truffle/reference/contract-abstractions), en lançant la console ainsi:
 ```sh
 $ npm run console
 ```
 
-Ou alors, on peut déployer le contrat sur un réseau _Rinkeby_, auquel cas il vous sera demandé d'entrer la clé privée à utiliser pour la transaction de déploiement (pour obtenir des ETH de test, passer par ce [faucet](https://faucet.rinkeby.io/)):
+Ou alors, il est possible de déployer le contrat sur un réseau de test public _Rinkeby_, auquel cas il vous sera demandé d'entrer la clé privée à utiliser pour la transaction de déploiement (pour obtenir des ETH de test, passer par ce [faucet](https://faucet.rinkeby.io/)):
 ```sh
 $ npm run deploy
 ```
-On peut ensuite interagir avec le contrat via la librairie JavaScript [`web3js`](https://web3js.readthedocs.io/en/v1.3.0/), que ce soit en ligne de commande via la console `node`, ou via une interface utilisateur développée en HTML5.
+On peut ensuite interagir avec le contrat via la librairie JavaScript [`web3js`](https://web3js.readthedocs.io/en/v1.3.0/), que ce soit en ligne de commande via la console `node`, ou via une interface utilisateur développée en HTML5 (non fournie ici).  
+Pour une interaction depuis un script python, on peut utiliser la librairie [Web3.py](https://web3py.readthedocs.io/en/latest/).  
+En tous les cas, le descripteur [ABI](https://solidity.readthedocs.io/en/v0.6.12/abi-spec.html#index-0), nécessaire à la connexion avec l'instance déployée du contrat, se trouve dans le fichier `build/contracts/Minidao.json` qui aura été généré avant le déploiement.
--- contracts/Minidao.sol
+++ contracts/Minidao.sol
@@ -15,10 +15,14 @@
  *                                  |_|
  *******************************************************************************
 
-Demonstration contract 'Minidao'.
+Demonstration contract 'Minidao'. French documented as it was written as part of
+a course about smart contracts and DAOs.
 
 /!\ NO SECURITY AUDIT WAS CONDUCTED. /!\
-/!\ DO NOT USE ON MAIN NET /!\
+/!\ DEMONSTRATION PURPOSE ONLY /!\
+/!\ USE ON MAIN NET AT YOUR OWN RISK /!\
+/!\ This contract should probably use a withdrawal pattern for its beneficiaries
+to be payed, instead of the implemented distribution method.
 
 Author: Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
 
@@ -39,22 +43,152 @@ SOFTWARE.
 ***************************************************************************** */
 pragma solidity ^0.6.3;
 
-import "fbo-contracts-lib/contracts/lib/Administre.sol";
+import "./lib/Administre.sol";
 
+/// @title Minidao
+/// @dev Hérite des méthodes de la dépendance `Administre` (contrat dans le sous-répertoire `lib`), qui gère les droits d'accès et niveaux d'autorisations.
+/// @notice Ce contrat agit comme une "mini-DAO", capable de gérer une "cagnotte", qui sera distribuée (maximum une fois par semaine) aux bénéficiaires désignés, selon une répartition proportionnelle aux votes des participants.
 contract Minidao is Administre {
-	
+
+	string constant private ERREUR_VOTANT_NON_AUTORISE = "VOTANT NON AUTORISÉ";
+	string constant private ERREUR_MEMBRE_COMITE_INACTIF = "MEMBRE COMITÉ INACTIF";
+	string constant private ERREUR_DOUBLE_APPROBATION = "DEJA APPROUVÉ";
+	string constant private ERREUR_BENEFICIAIRE_INVALIDE = "BÉNÉFICIAIRE INVALIDE";
+	string constant private ERREUR_INTERVALLE_DISTRIBUTION = "UNE SEMAINE NON ÉCOULÉE";
+
+	// Définition du niveau d'autorisation Membre Comité
 	uint8 constant internal AUTORISATION_COMITE = 0xB0;
-	uint8 constant internal AUTORISATION_VOTANT = 0xA0;
 
-	event NouveauMembreComite(indexed address id);
-	event NouveauVotant(indexed address id);
+	// "Events" de journalisation de certains changements.
+	event NouveauMembreComite(address indexed id);
+	event NouveauVotant(address indexed id);
+	event NouveauBeneficiaire(address indexed id);
+	event NouveauScrutin(uint indexed num, uint distribue); // renvoie le nouveau numéro de scrutin, ainsi que le montant total distribué à l'issue du scrutin précédent.
+	event VoteFinActivite(address indexed id, uint8 restant); // renvoie l'adresse-id du Membre au Comité qui a voté la fin d'activité, ainsi que le nombre de votes restants nécessaires pour déclencher la cessation d'activité.
 
-	mapping (address => uint32) votes;
+	mapping (address => mapping (address => bool)) approbVotant; // Approbation des Membres du Comité pour les votants
+	mapping (address => mapping (address => bool)) approbBenef; // Approbation des Membres au Comité pour les bénéficiaires
+	mapping (address => uint8) votant; // Autorisation des votants
+	mapping (address => uint8) benef; // Autorisation des bénéficiaires
+	mapping (address => bool) voteFin; // Marques de vote de fin d'activité des Membres au Comité
+	mapping (address => mapping (uint => uint32)) votes; // Décomptes de votes pour les bénéficiaires
+	address[] public beneficiaires; // Liste de bénéficiaires approuvés.
 
-	function ajouteMembreComite(address id) {
-		// TODO: continue !
+	uint8 public membresComite = 0; // Nombre de Membres au Comité
+	uint public cagnotte = 0; // Montant de la cagnotte
+	uint public scrutin = 1; // Numéro du scrutin en cours
+	uint public distribPrecedente; // Timestamp de la dernière distribution ayant été exécutée.
+	bool public actif = true;
+
+	/// @dev Au déploiement du contrat, le "déployeur" est automatiquement ajouté comme premier Membre au Comité.
+	constructor() public {
+		ajouteMembreComite(msg.sender);
 	}
 
-	// TODO: continue !
-	
+	/// @dev Toute valeur (ETH uniquement) envoyée au contrat est ajoutée à la cagnotte.
+	receive() external payable {
+		cagnotte += msg.value;
+	}
+
+	modifier seulActif() {
+		require(actif, "CONTRAT INACTIF");
+		_;
+	}
+
+	modifier membreActif() {
+		require(!voteFin[msg.sender], ERREUR_MEMBRE_COMITE_INACTIF);
+		_;
+	}
+
+	modifier seulVotant() {
+		require(estVotant(msg.sender), ERREUR_VOTANT_NON_AUTORISE);
+		_;
+	}
+
+	/// @dev Méthode de test d'autorisation d'un votant.
+	/// @param id L'adresse-identité du votant à tester.
+	/// @return `true` si le votant est approuvé.
+	function estVotant(address id) internal view returns (bool) {
+		return votant[id] >= membresComite / 2;
+	}
+
+	/// @dev Méthode d'ajout d'un membre au Comité qui peut accepter de nouveaux votants. Seul l'administrateur du contrat peut le faire.
+	/// @param id L'adresse-identité du nouveau Membre au Comité.
+	function ajouteMembreComite(address id) public seulAdmin seulActif {
+		require(membresComite <= 10, "NB_MEMBRES_COMITE_MAX");
+		autorise(id, AUTORISATION_COMITE);
+		++membresComite;
+		emit NouveauMembreComite(id);
+	}
+
+	/// @notice Approbation d'un votant. Seul un Membre au Comité peut le faire. Le votant ne sera approuvé qu'une fois une majorité de 50% atteinte.
+	/// @param id L'adresse-identité du votant à approuver.
+	function approuveVotant(address id) public seulAutorise(AUTORISATION_COMITE) seulActif membreActif {
+		if (approbVotant[msg.sender][id] || votant[id] >= membresComite / 2) {
+			revert(ERREUR_DOUBLE_APPROBATION);
+		}
+		approbVotant[msg.sender][id] = true;
+		if (++votant[id] >= membresComite / 2) {
+			emit NouveauVotant(id);
+		}
+	}
+
+	/// @notice Approbation d'un bénéficiaire. Seul un Membre au Comité peut le faire. Le bénéficiaire ne sera approuvé qu'une fois l'unanimité atteinte.
+	/// @param id L'adresse-identité du bénéficiaire à approuver.
+	function approuveBeneficiaire(address id) public seulAutorise(AUTORISATION_COMITE) seulActif membreActif {
+		if (approbBenef[msg.sender][id] || benef[id] >= membresComite) {
+			revert(ERREUR_DOUBLE_APPROBATION);
+		}
+		approbBenef[msg.sender][id] = true;
+		if (++benef[id] >= membresComite) {
+			beneficiaires.push(id);
+			emit NouveauBeneficiaire(id);
+		}
+	}
+
+	/// @notice Méthode de vote
+	/// @param beneficiaire L'adresse-identité du bénéficiaire auquel donner son vote.
+	function vote(address beneficiaire) public seulVotant seulActif {
+		require(benef[beneficiaire] >= membresComite, ERREUR_BENEFICIAIRE_INVALIDE);
+		++votes[beneficiaire][scrutin];
+	}
+
+	/// @notice Méthode de déclenchement de la distribution hebdomadaire des fonds. Seul un Membre au Comité peut le faire.
+	/// @param frais Montant (en Wei) demandé en remboursement de frais et émolument. Maximum 0.1 ETH ou 10% du montant de la cagnotte.
+	function distribution(uint frais) public seulAutorise(AUTORISATION_COMITE) seulActif membreActif {
+		require(block.timestamp >= distribPrecedente + 7 days, ERREUR_INTERVALLE_DISTRIBUTION);
+		uint rembourseMax = cagnotte / 100;
+		rembourseMax = rembourseMax > 100 finney ? 100 finney : rembourseMax;
+		uint rembourse = frais > rembourseMax ? rembourseMax : frais;
+		uint aDistribuer = cagnotte - rembourse;
+		uint totalDistribue = 0;
+		uint nbVotesTotal = 0;
+		uint montant;
+		uint32 i = 0;
+		while (i < beneficiaires.length) {
+			nbVotesTotal += votes[beneficiaires[i++]][scrutin];
+		}
+		i = 0;
+		while (i < beneficiaires.length) {
+			montant = (votes[beneficiaires[i]][scrutin] * aDistribuer) / nbVotesTotal;
+			cagnotte -= montant;
+			payable(beneficiaires[i++]).transfer(montant);
+			totalDistribue += montant;
+		}
+		emit NouveauScrutin(++scrutin, totalDistribue);
+		montant = cagnotte > rembourse ? rembourse : cagnotte;
+		cagnotte -= montant;
+		msg.sender.transfer(montant);
+	}
+
+	/// @notice Méthode de vote des Membres au Comité pour cesser les activités. Nécessite l'unanimité.
+	function termine() public seulAutorise(AUTORISATION_COMITE) seulActif membreActif {
+		voteFin[msg.sender] = true;
+		if (--membresComite == 0) {
+			cagnotte = 0;
+			actif = false;
+			payable(administrateur()).transfer(address(this).balance);
+		}
+		emit VoteFinActivite(msg.sender, membresComite);
+	}
 }
